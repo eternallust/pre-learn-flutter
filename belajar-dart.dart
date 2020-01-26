cc3bcc81efaa2ce5ae5ebbbda35ae49f45ae42db
eternallust <rizkyabdulrachman@gmail.com>
2020-01-26 22:53:57 +0700

    Add files via upload

---
+++ belajar-dart.dart
@@ -0,0 +1,58 @@
+void main(){
+  var deck = new Deck();
+//   deck.shuffle();
+//   print(deck);
+  
+//   print(deck.deal(5));
+//   print(deck);
+  
+  print(deck);
+  print(deck.removeCard('Diamonds','Ace'));
+}
+
+class Deck {
+  List<Card> cards = [];
+  var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+  var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+  
+  Deck(){
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank,suit);
+        cards.add(card);
+      }
+    }
+  }
+  toString(){
+    return cards.toString();
+  }
+  shuffle(){
+    cards.shuffle();
+  }
+  cardWithSuit(String suit){
+    return cards.where((card) => card.suit == suit);
+  }
+  deal(int handSize) {
+    var hand = cards.sublist(0,handSize);
+    cards = cards.sublist(handSize);
+    return hand;
+  }
+  removeCard(String suit, String rank){
+//     cards.removeWhere((card) => (card.suit == suit) && (card.rank == rank));
+    cards.removeWhere((item){
+      return item.suit == suit && item.rank == rank;
+    });
+  }
+}
+
+
+class Card {
+  String suit;
+  String rank;
+  
+  Card(this.rank,this.suit);
+  
+  toString(){
+    return "$rank of $suit";
+  }
+}
